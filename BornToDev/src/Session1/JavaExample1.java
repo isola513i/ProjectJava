ff9784bc2f401b6b1ed0dd8503dc62b3870a8f4e
isola513i <okuma530@gmail.com>
2023-10-03 23:09:06 +0700

    adding files

--- BornToDev/src/Session1/JavaExample1.java
+++ BornToDev/src/Session1/JavaExample1.java
@@ -2,6 +2,6 @@ package Session1;
 
 public class JavaExample1 {
     public static void main(String[] args) {
-
+        System.out.println("Hello Git");
     }
 }
--- Web Technology/Borntodev JS/Project 2/app.js
+++
@@ -1,4 +0,0 @@
-let age = prompt("กรุณากรอกอายุของท่าน")
-if (age > 13) {
-    document.getElementById("content").innerHTML = "ไอควาย"
-}
