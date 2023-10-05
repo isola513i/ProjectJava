253360e026c07fc8899e23694d2ee9352d37accc
isola513i <okuma530@gmail.com>
2023-10-05 20:59:56 +0700

    10/5/2023

--- BornToDev/src/Session1/JavaExample1.java
+++
@@ -1,7 +0,0 @@
-package Session1;
-
-public class JavaExample1 {
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
-    }
-}
---
+++ BornToDev/src/Session1/JavaExample2.java
@@ -0,0 +1,9 @@
+package Session1;
+
+public class JavaExample2 {
+    public static void main(String[] args) {
+        System.out.println(10+5); //int
+        System.out.println(3.14f); //float
+        System.out.println(0.55555555555); //double
+    }
+}
