c348e902b7bd6079199593451f8c57d480531a9b
isola513i <okuma530@gmail.com>
2023-10-05 21:34:09 +0700

    10/5/2023

---
+++ BornToDev/src/Session1/JavaExample3.java
@@ -0,0 +1,15 @@
+package Session1;
+
+public class JavaExample3 {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 5;
+        double money = 10000.25;
+        System.out.println(money);
+        money = 1200;
+        System.out.println(x+y);
+        System.out.println(money);
+        String open = "Hello I Love You";
+        System.out.println(open);
+    }
+}
