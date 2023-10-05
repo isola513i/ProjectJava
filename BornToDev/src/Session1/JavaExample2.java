841cefcb0e7dd8224e54d240e0655eaa5dffec4a
isola513i <okuma530@gmail.com>
2023-10-05 21:16:19 +0700

    10/5/2023

--- BornToDev/src/Session1/JavaExample2.java
+++ BornToDev/src/Session1/JavaExample2.java
@@ -6,5 +6,13 @@ public class JavaExample2 {
         long nl = 10000;
         float f = 3.14f;
         double d = 1.55555555555;
+        boolean IsProudCute = true;
+        System.out.println("Proud is so cute = " + IsProudCute);
+        System.out.print('I');
+        System.out.print('L');
+        System.out.println('P');
+        System.out.println(nl);
+
+
     }
 }
